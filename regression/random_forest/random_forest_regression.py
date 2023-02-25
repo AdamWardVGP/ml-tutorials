69f072d2ca20c92f76a9f82af6a1316438399ade
Adam W <AdamWardVGP@gmail.com>
2023-02-24 23:07:58 -0800

    Random Forest Regression

--- regression/random_forest/random_forest_regression.py
+++ regression/random_forest/random_forest_regression.py
@@ -1,5 +1,5 @@
 import pandas
-from sklearn import tree
+from sklearn import ensemble
 import numpy
 from matplotlib import pyplot
 
@@ -9,18 +9,18 @@ dataset = pandas.read_csv("Position_Salaries.csv")
 x = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
-classifier = tree.DecisionTreeClassifier(random_state=1)
-classifier.fit(x, y)
+regressor = ensemble.RandomForestRegressor(n_estimators=10, random_state=0)
+regressor.fit(x, y)
 
-predict_y = classifier.predict([[6.5]])
+predict_y = regressor.predict([[6.5]])
 print(f'predicted salary {predict_y}')
 
 x_grid = numpy.arange(min(x), max(x), 0.1)
 x_grid = x_grid.reshape((len(x_grid), 1))
 pyplot.scatter(x, y, color='red')
-pyplot.plot(x_grid, classifier.predict(x_grid), color='blue')
+pyplot.plot(x_grid, regressor.predict(x_grid), color='blue')
 pyplot.scatter([[6.5]], [[predict_y]], color='green')
-pyplot.title('Truth or Bluff (Decision Tree Regression)')
+pyplot.title('Truth or Bluff (Random Forest Regression)')
 pyplot.xlabel('Position level')
 pyplot.ylabel('Salary')
 pyplot.show()
