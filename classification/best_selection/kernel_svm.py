4d23ce867b03317bab08e2456ec8f231e3ae4441
Adam W <AdamWardVGP@gmail.com>
2023-03-02 14:53:22 -0800

    Classification Model Comparisons

---
+++ classification/best_selection/kernel_svm.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+classifier = SVC(kernel='rbf', random_state=0)
+classifier.fit(x_train, y_train)
+
+y_predicted = classifier.predict(x_test)
+cm = confusion_matrix(y_test, y_predicted)
+print(cm)
+print(accuracy_score(y_test, y_predicted))
